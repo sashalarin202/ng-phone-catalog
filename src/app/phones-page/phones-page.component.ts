77f1a90af144fb6b372b5c2c5e2bbf6f6dad84cc
Oleksandr Larin <Oleksandr.Larin@infopulse.com>
2021-07-07 23:31:31 +0300

    home&phones was added

---
+++ src/app/phones-page/phones-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phones-page',
+  templateUrl: './phones-page.component.html',
+  styleUrls: ['./phones-page.component.scss']
+})
+export class PhonesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
